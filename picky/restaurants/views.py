59d32f0c06dc63c89ffe6fcca179c76c3f1696c2
crazdrms <dilipmaharjann@gmail.com>
2017-11-26 22:37:11 -0500

    override context

--- picky/restaurants/views.py
+++ picky/restaurants/views.py
@@ -4,10 +4,19 @@ from django.views.generic import TemplateView
 class HomeView(TemplateView):
     template_name = 'home.html'
 
+    def get_context_data(self, *args, **kwargs):
+        return {"title": "Home"}
+
 
 class AboutView(TemplateView):
     template_name = 'about.html'
 
+    def get_context_data(self, *args, **kwargs):
+        return {"title": "About"}
+
 
 class ContactView(TemplateView):
     template_name = 'contact.html'
+
+    def get_context_data(self, *args, **kwargs):
+        return {"title": "Contact"}
