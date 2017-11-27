5f17eb61156d376ee166cba07c0faa21e7cd19dc
crazdrms <dilipmaharjann@gmail.com>
2017-11-26 20:00:31 -0500

    html response

--- restaurants/views.py
+++ restaurants/views.py
@@ -5,4 +5,17 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Django 1.11.2")
+    html_ = """
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Title</title>
+    </head>
+    <body>
+        <h1>Django 1.11.2</h1>
+    </body>
+    </html>
+    """
+    # return HttpResponse("Django 1.11.2")
+    return HttpResponse(html_)
