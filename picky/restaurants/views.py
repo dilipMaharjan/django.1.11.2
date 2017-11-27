bdf5840a0580736b43ece6a198a0978c2d1ff42b
crazdrms <dilipmaharjann@gmail.com>
2017-11-26 21:58:14 -0500

    template inheritance, include

--- picky/restaurants/views.py
+++ picky/restaurants/views.py
@@ -2,4 +2,12 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'base.html', {"context": "context"})
+    return render(request, 'home.html', {"title": "Home"})
+
+
+def about(request):
+    return render(request, 'about.html', {"title": "About"})
+
+
+def contact(request):
+    return render(request, 'contact.html', {"title": "Contact"})
