41b6d19cba5256317726e9bbee59b109028306aa
crazdrms <dilipmaharjann@gmail.com>
2017-11-28 21:54:17 -0500

    slug

--- picky/restaurants/models.py
+++ picky/restaurants/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.db.models.signals import pre_save, post_save
+
+from .utils import unique_slug_generator
 
 
 class RestaurantLocation(models.Model):
@@ -7,6 +10,26 @@ class RestaurantLocation(models.Model):
     category = models.CharField(max_length=120, null=True, blank=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(unique=True, blank=True)
 
     def __str__(self):
         return self.name
+
+    @property
+    def title(self):
+        return self.name
+
+
+# def rl_post_save_receiver(self, sender, instance,create, *args, **kwargs):
+#     if not instance.slug:
+#         instance.slug = unique_slug_generator(instance)
+#         instance.save()
+
+
+def rl_pre_save_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
+
+
+pre_save.connect(rl_pre_save_receiver, sender=RestaurantLocation)
+# post_save.connect(rl_post_save_receiver, sender=RestaurantLocation)
