589f6ce5cd8a7c9324e86d094285c913a27b653f
crazdrms <dilipmaharjann@gmail.com>
2017-11-30 18:14:23 -0500

    detail view fixed

--- picky/restaurants/views.py
+++ picky/restaurants/views.py
@@ -1,8 +1,9 @@
 from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
 from .models import RestaurantLocation
-from django.views.generic import ListView, DetailView,CreateView
+from django.views.generic import ListView, DetailView, CreateView
 from .form import RestaurantLocationForm
+
 
 def restaurant_list(request):
     template_name = 'restaurants/restaurantlocation_list.html'
@@ -24,7 +25,7 @@ class RestaurantListView(ListView):
 
 
 class RestaurantDetailView(DetailView):
-    queryset = RestaurantLocation.objects.filter(slug=)
+    queryset = RestaurantLocation.objects.all()
 
     # def get_context_data(self, *args, **kwargs):
     #     return super(RestaurantDetailView, self).get_context_data(*args, **kwargs)
@@ -33,7 +34,8 @@ class RestaurantDetailView(DetailView):
     #     slug = self.kwargs.get('slug')
     #     return get_object_or_404(RestaurantLocation, slug=slug)
 
+
 class RestaurantLocationCreateView(CreateView):
     form_class = RestaurantLocationForm
     template_name = "restaurants/form.html"
-    success_url="/"
+    success_url = "/"
